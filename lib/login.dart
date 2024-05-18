b393343f27ce49da6a6d4568dffb7cf0d2eee473
Varun Shukla <vslpahm@gmail.com>
2024-05-18 10:45:18 +0530

    login colour

--- lib/login.dart
+++ lib/login.dart
@@ -10,33 +10,47 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.red,
+              Colors.black
+            ]
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const TextField(
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(color: Colors.white),
+                  labelText: 'Username',
+                ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
+              const SizedBox(height: 20.0),
+              const TextField(
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(color: Colors.white),
+                  labelText: 'Password',
+                ),
+                obscureText: true,
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Implement login functionality here
-                // For example, validate username and password
-                // and navigate to the next screen if valid
-              },
-              child: const Text('Submit'),
-            ),
-          ],
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement login functionality here
+                  // For example, validate username and password
+                  // and navigate to the next screen if valid
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
