b619015a1d6465a0e65fa66624b1488758e7a0bd
Varun Shukla <vslpahm@gmail.com>
2024-06-30 11:28:53 +0800

    entrypoint update

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,5 +3,11 @@
 # Ensure the Dart SDK is in the PATH
 export PATH=$PATH:/app/dart-sdk/bin
 
+# Ensure PUBSPEC_PATH is set
+export PUBSPEC_PATH=${PUBSPEC_PATH:-/app/pubspec.yaml}
+
+# Run pub get using the specified pubspec.yaml
+pub get --directory=${PUBSPEC_PATH%/*}
+
 # Run your Dart application
 dart main.dart
