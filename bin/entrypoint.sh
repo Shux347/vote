5fb6607595881cedba98f845aa5ad00953272332
Varun Shukla <vslpahm@gmail.com>
2024-06-30 11:38:05 +0800

    entry

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,7 +3,7 @@
 # Ensure the Dart SDK is in the PATH
 export PATH=$PATH:/app/dart-sdk/bin
 
-# Ensure PUBSPEC_PATH is set
+# Set PUBSPEC_PATH if not set
 export PUBSPEC_PATH=${PUBSPEC_PATH:-/app/pubspec.yaml}
 
 # Run pub get using the specified pubspec.yaml
